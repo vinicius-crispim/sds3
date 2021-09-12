1050885bf92cf014077275f87dc82304ff2cbc66
Vinicius Crispim <vinicrispim02@hotmail.com>
2021-09-12 00:13:00 -0300

    Layers

---
+++ backend/src/main/java/com/coursedev/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.coursedev.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coursedev.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/coursedev/dsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.coursedev.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coursedev.dsvendas.dto.SellerDTO;
+import com.coursedev.dsvendas.entities.Seller;
+import com.coursedev.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+	
+}
